bf840ec34947186ad20b7c44af8a44bceb64ae64
rinjhasijapati <aastha_sijapati@yahoo.com>
2023-08-26 11:27:01 +0545

    second commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import Logo from '../assets/logo.png';
 import { Link } from 'react-router-dom';
+import ReorderIcon from '@mui/icons-material/Reorder';
+import '../styles/Navbar.css';
 
 function Navbar() {
   return (
     <div className='navbar'>
         <div className='leftSide'></div>
             <img src={Logo} />
-        <div className='rightSide'></div>
+        <div className='rightSide'>
             <Link to="/">Home</Link>
             <Link to="/menu">Menu</Link>
             <Link to="/about">About</Link>
             <Link to="/conatct">Contact</Link>
+            <button>
+              <ReorderIcon /> 
+            </button>
+        </div>
     </div>
   )
 }
